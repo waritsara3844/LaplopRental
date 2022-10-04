5c74d4b3ef23704e5ca0ff9f9050f4726a3bd6f1
waritsara3844 <s6330613011@phuket.psu.ac.th>
2022-10-04 17:33:40 +0700

    changing

--- TermProject/src/pages/RentalLaptopPage.vue
+++ TermProject/src/pages/RentalLaptopPage.vue
@@ -1,15 +1,18 @@
 <template>
   <g-page>
     <h6>this is RentalLaptop</h6>
+    <q-card>
+      <q-card-section>
+        <q-icon name="avatar"></q-icon>
+      </q-card-section>
+    </q-card>
   </g-page>
 </template>
 
 <script>
 export default {
-  neme:"RentalLaptopPage"
-}
+  neme: "RentalLaptopPage",
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
